ab3a10c48246965dfe2f23b5b27599cf56b04ae4
Sam Bruton <samuel.bruton@ca.vu>
2025-10-22 09:35:26 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-layout">
+    <Sidebar />
+    <MainContent />
+  </div>
+</template>
+
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+import MainContent from './components/MainContent.vue';
+</script>
